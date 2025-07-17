2c60e45a39159643828fab9859dfa692280df202
Ian Zurutuza <ian.zurutuza@gmail.com>
2025-07-17 02:23:17 -0500

    init

---
+++ README.md
@@ -0,0 +1,136 @@
+# CVG Footpedal
+
+This device transforms a usb footpedal into a wireless (bluetooth) mouse.
+The usb footpedal reports as a keyboard with keys: ["a", "b", "c"]).
+The device communicates with the keyboard and when connected to a
+bluetooth host identifies as an HID compliant mouse sends pedal presses
+as mouse clicks (left, middle, and right).
+
+## Device Setup
+
+### Software Required
+
+- **TODO: link to esp-idf setup**
+
+> See the [Getting Started Guide](https://idf.espressif.com/) for full
+steps to configure and use ESP-IDF to build projects.
+
+### Hardware Required
+
+- ESP32 SoC DEV BOARD [ESP32-S3-USB-OTG](https://docs.espressif.com/projects/esp-dev-kits/en/latest/esp32s3/esp32-s3-usb-otg/index.html)
+- A micro-usb cable for power supply and programming
+- **TODO: all the rest of the components required**
+
+**How much does it cost?**
+bill of materials
+
+### TODO: Assembly
+
+1. assembly instructions
+2. picture of assembled device
+
+### Build and Flash
+
+Build the project and flash it to the board, then run monitor tool to
+view serial output.
+
+```sh
+idf.py -p PORT flash monitor
+```
+
+> (To exit the serial monitor, type ``Ctrl-]``.)
+
+### TODO: pairing instructions
+
+## Troubleshooting
+
+### TODO: any tips if the device is not connecting
+
+### From Espressif
+
+1. When using NimBLE stack, some iOS devices do not show the volume pop up.
+To fix this, please set CONFIG_BT_NIMBLE_SM_LVL to value 2.
+iOS needs Authenticated Pairing with Encryption to show up the pop ups.
+
+## Example Output
+
+```
+I (353) BLE_INIT: Using main XTAL as clock source
+I (363) BLE_INIT: Feature Config, ADV:1, BLE_50:1, DTM:1, SCAN:1, CCA:0, SMP:1, CONNECT:1
+I (373) BLE_INIT: Bluetooth MAC: f4:12:fa:9b:64:2a
+I (373) phy_init: phy_version 701,f4f1da3a,Mar  3 2025,15:50:10
+I (413) HID_DEV_DEMO: setting ble device
+I (423) HID_DEV_DEMO: BLE Host Task Started
+I (423) NimBLE: GAP procedure initiated: stop advertising.
+
+I (423) HID_DEV_BLE: START
+I (423) main_task: Returned from app_main()
+I (423) NimBLE: GAP procedure initiated: advertise; 
+I (433) NimBLE: disc_mode=2
+I (433) NimBLE:  adv_channel_map=0 own_addr_type=0 adv_filter_policy=0 adv_itvl_min=48 adv_itvl_max=80
+I (443) NimBLE: 
+
+I (5293) HID_DEV_BLE: CONNECT
+I (5293) ESP_HID_GAP: connection established; status=0
+I (5483) NimBLE: encryption change event; status=0 
+########################################################################
+BT hid mouse demo usage:
+This auto clicks the left, right, and mouse wheel buttons
+########################################################################
+
+I (5503) main task: LEFT CLICK
+
+I (5503) NimBLE: GATT procedure initiated: notify; 
+I (5513) NimBLE: att_handle=37
+
+I (5513) NimBLE: notify_tx event; conn_handle=1 attr_handle=37 status=0 is_indication=0
+I (5523) ESP_HID_GAP: subscribe event; conn_handle=1 attr_handle=8 reason=3 prevn=0 curn=0 previ=0 curi=1
+
+I (5533) main task: RELEASE
+I (5533) NimBLE: GATT procedure initiated: notify; 
+I (5533) NimBLE: att_handle=37
+
+I (5543) NimBLE: notify_tx event; conn_handle=1 attr_handle=37 status=0 is_indication=0
+I (5543) ESP_HID_GAP: subscribe event; conn_handle=1 attr_handle=18 reason=3 prevn=0 curn=1 previ=0 curi=0
+
+I (5553) ESP_HID_GAP: subscribe event; conn_handle=1 attr_handle=37 reason=3 prevn=0 curn=1 previ=0 curi=0
+
+I (5583) ESP_HID_GAP: mtu update event; conn_handle=1 cid=4 mtu=256
+I (6543) main task: RIGHT CLICK
+
+I (6543) NimBLE: GATT procedure initiated: notify; 
+I (6543) NimBLE: att_handle=37
+
+I (6543) NimBLE: notify_tx event; conn_handle=1 attr_handle=37 status=0 is_indication=0
+I (6553) main task: RELEASE
+I (6553) NimBLE: GATT procedure initiated: notify; 
+I (6553) NimBLE: att_handle=37
+
+I (6553) NimBLE: notify_tx event; conn_handle=1 attr_handle=37 status=0 is_indication=0
+I (7563) main task: MIDDLE CLICK
+
+I (7563) NimBLE: GATT procedure initiated: notify; 
+I (7563) NimBLE: att_handle=37
+
+I (7563) NimBLE: notify_tx event; conn_handle=1 attr_handle=37 status=0 is_indication=0
+I (7573) main task: RELEASE
+I (7573) NimBLE: GATT procedure initiated: notify; 
+I (7573) NimBLE: att_handle=37
+...
+
+I (14713) NimBLE: notify_tx event; conn_handle=1 attr_handle=37 status=0 is_indication=0
+I (15093) ESP_HID_GAP: subscribe event; conn_handle=1 attr_handle=8 reason=2 prevn=0 curn=0 previ=1 curi=0
+
+I (15093) ESP_HID_GAP: subscribe event; conn_handle=1 attr_handle=18 reason=2 prevn=1 curn=0 previ=0 curi=0
+
+I (15103) ESP_HID_GAP: subscribe event; conn_handle=1 attr_handle=37 reason=2 prevn=1 curn=0 previ=0 curi=0
+
+I (15113) ESP_HID_GAP: disconnect; reason=531
+I (15113) HID_DEV_BLE: DISCONNECT: UNKNOWN
+I (15113) NimBLE: GAP procedure initiated: advertise; 
+I (15123) NimBLE: disc_mode=2
+I (15123) NimBLE:  adv_channel_map=0 own_addr_type=0 adv_filter_policy=0 adv_itvl_min=48 adv_itvl_max=80
+I (15133) NimBLE: 
+```
+
+
---
+++ main/footpedal_main.c
@@ -0,0 +1,272 @@
+/*
+ * Modified esp_hid_device example
+ * June 2025 - Ian Zurutuza
+ *
+ * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
+
+#include "host/ble_hs.h"
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+
+#include "esp_hidd.h"
+#include "esp_hid_gap.h"
+#include "portmacro.h"
+
+static const char *TAG = "HID_DEV_DEMO";
+
+typedef struct
+{
+    TaskHandle_t task_hdl;
+    esp_hidd_dev_t *hid_dev;
+    uint8_t protocol_mode;
+    uint8_t *buffer;
+} local_param_t;
+
+static local_param_t s_ble_hid_param = {0};
+
+const unsigned char mouseReportMap[] = {
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x02,                    // USAGE (Mouse)
+    0xa1, 0x01,                    // COLLECTION (Application)
+
+    0x09, 0x01,                    //   USAGE (Pointer)
+    0xa1, 0x00,                    //   COLLECTION (Physical)
+
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
+    0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x95, 0x01,                    //     REPORT_COUNT (1)
+    0x75, 0x05,                    //     REPORT_SIZE (5)
+    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
+
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x30,                    //     USAGE (X)
+    0x09, 0x31,                    //     USAGE (Y)
+    0x09, 0x38,                    //     USAGE (Wheel)
+    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
+    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
+    0x75, 0x08,                    //     REPORT_SIZE (8)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x81, 0x06,                    //     INPUT (Data,Var,Rel)
+
+    0xc0,                          //   END_COLLECTION
+    0xc0                           // END_COLLECTION
+};
+
+// send the buttons, change in x, and change in y
+void send_mouse(uint8_t buttons, char dx, char dy, char wheel)
+{
+    static uint8_t buffer[4] = {0};
+    buffer[0] = buttons;
+    buffer[1] = dx;
+    buffer[2] = dy;
+    buffer[3] = wheel;
+    esp_hidd_dev_input_set(s_ble_hid_param.hid_dev, 0, 0, buffer, 4);
+}
+
+void ble_hid_demo_task_mouse(void *pvParameters)
+{
+    static const char* help_string = "########################################################################\n"\
+    "BT hid mouse demo usage:\n"\
+    "This auto clicks the left, right, and mouse wheel buttons\n"\
+    "########################################################################\n";
+    printf("%s\n", help_string);
+
+    int i = 0;
+
+    const TickType_t delay = 1000 / portTICK_PERIOD_MS;
+    const TickType_t delaySmall = 10 / portTICK_PERIOD_MS;
+
+    while (1) {
+        // FIXME: leave this loop empty (except a delay)
+        // Or never start it
+
+        if ( i == 0 ) {
+            // left
+            ESP_LOGI("main task", "LEFT CLICK\n");
+            send_mouse(1, 0, 0, 0);
+        } else if ( i == 1 ) {
+            // right
+            ESP_LOGI("main task", "RIGHT CLICK\n");
+            send_mouse(2, 0, 0, 0);
+        } else if ( i == 2 ) {
+            // middle
+            ESP_LOGI("main task", "MIDDLE CLICK\n");
+            send_mouse(4, 0, 0, 0);
+        }
+
+        i += 1;
+        if (i == 3) i = 0;
+
+        vTaskDelay(delaySmall);
+        ESP_LOGI("main task", "RELEASE");
+        send_mouse(0, 0, 0, 0);
+
+        vTaskDelay(delay);
+    }
+}
+
+static esp_hid_raw_report_map_t ble_report_map[] = {
+    {
+        .data = mouseReportMap,
+        .len = sizeof(mouseReportMap)
+    },
+};
+
+// FIXME: set vendor and product id to an available one
+static esp_hid_device_config_t ble_hid_config = {
+    .vendor_id          = 0x16C0,
+    .product_id         = 0x05DF,
+    .version            = 0x0001,
+    .device_name        = "cvg-mouse",
+    .manufacturer_name  = "UT Southwestern",
+    .serial_number      = "0000000001",
+    .report_maps        = ble_report_map,
+    .report_maps_len    = 1
+};
+
+void ble_hid_task_start_up(void)
+{
+    if (s_ble_hid_param.task_hdl) {
+        // Task already exists
+        return;
+    }
+
+    /* Nimble Specific */
+    xTaskCreate(ble_hid_demo_task_mouse, "ble_hid_demo_task_mouse", 3 * 1024, NULL, configMAX_PRIORITIES - 3,
+                &s_ble_hid_param.task_hdl);
+}
+
+void ble_hid_task_shut_down(void)
+{
+    if (s_ble_hid_param.task_hdl) {
+        vTaskDelete(s_ble_hid_param.task_hdl);
+        s_ble_hid_param.task_hdl = NULL;
+    }
+}
+
+static void ble_hidd_event_callback(void *handler_args, esp_event_base_t base, int32_t id, void *event_data)
+{
+    esp_hidd_event_t event = (esp_hidd_event_t)id;
+    esp_hidd_event_data_t *param = (esp_hidd_event_data_t *)event_data;
+    static const char *TAG = "HID_DEV_BLE";
+
+    switch (event) {
+    case ESP_HIDD_START_EVENT: {
+        ESP_LOGI(TAG, "START");
+        esp_hid_ble_gap_adv_start();
+        break;
+    }
+    case ESP_HIDD_CONNECT_EVENT: {
+        ESP_LOGI(TAG, "CONNECT");
+        break;
+    }
+    case ESP_HIDD_PROTOCOL_MODE_EVENT: {
+        ESP_LOGI(TAG, "PROTOCOL MODE[%u]: %s", param->protocol_mode.map_index, param->protocol_mode.protocol_mode ? "REPORT" : "BOOT");
+        break;
+    }
+    case ESP_HIDD_CONTROL_EVENT: {
+        ESP_LOGI(TAG, "CONTROL[%u]: %sSUSPEND", param->control.map_index, param->control.control ? "EXIT_" : "");
+        if (param->control.control)
+        {
+            // exit suspend
+            // FIXME: or don't start a task, set a variable
+            ble_hid_task_start_up();
+        } else {
+            // suspend
+            ble_hid_task_shut_down();
+        }
+    break;
+    }
+    case ESP_HIDD_OUTPUT_EVENT: {
+        ESP_LOGI(TAG, "OUTPUT[%u]: %8s ID: %2u, Len: %d, Data:", param->output.map_index, esp_hid_usage_str(param->output.usage), param->output.report_id, param->output.length);
+        ESP_LOG_BUFFER_HEX(TAG, param->output.data, param->output.length);
+        break;
+    }
+    case ESP_HIDD_FEATURE_EVENT: {
+        ESP_LOGI(TAG, "FEATURE[%u]: %8s ID: %2u, Len: %d, Data:", param->feature.map_index, esp_hid_usage_str(param->feature.usage), param->feature.report_id, param->feature.length);
+        ESP_LOG_BUFFER_HEX(TAG, param->feature.data, param->feature.length);
+        break;
+    }
+    case ESP_HIDD_DISCONNECT_EVENT: {
+        ESP_LOGI(TAG, "DISCONNECT: %s", esp_hid_disconnect_reason_str(esp_hidd_dev_transport_get(param->disconnect.dev), param->disconnect.reason));
+        ble_hid_task_shut_down();
+        esp_hid_ble_gap_adv_start();
+        break;
+    }
+    case ESP_HIDD_STOP_EVENT: {
+        ESP_LOGI(TAG, "STOP");
+        break;
+    }
+    default:
+        break;
+    }
+    return;
+}
+
+void ble_hid_device_host_task(void *param)
+{
+    ESP_LOGI(TAG, "BLE Host Task Started");
+    /* This function will return only when nimble_port_stop() is executed */
+    nimble_port_run();
+
+    nimble_port_freertos_deinit();
+}
+void ble_store_config_init(void);
+
+void app_main(void)
+{
+    esp_err_t ret;
+
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    ESP_LOGI(TAG, "setting hid gap, mode:%d", HID_DEV_MODE);
+    ret = esp_hid_gap_init(HID_DEV_MODE);
+    ESP_ERROR_CHECK( ret );
+
+    ret = esp_hid_ble_gap_adv_init(ESP_HID_APPEARANCE_MOUSE, ble_hid_config.device_name);
+    ESP_ERROR_CHECK( ret );
+
+    ESP_LOGI(TAG, "setting ble device");
+    ESP_ERROR_CHECK(
+        esp_hidd_dev_init(&ble_hid_config, ESP_HID_TRANSPORT_BLE, ble_hidd_event_callback, &s_ble_hid_param.hid_dev));
+
+    ble_store_config_init();
+
+    ble_hs_cfg.store_status_cb = ble_store_util_status_rr;
+
+	/* Starting nimble task after gatts is initialized*/
+    ret = esp_nimble_enable(ble_hid_device_host_task);
+    if (ret) {
+        ESP_LOGE(TAG, "esp_nimble_enable failed: %d", ret);
+    }
+}
